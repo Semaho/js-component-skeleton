0c1a8b5946030708a7a6ae9277889168f2c6869e
Sébastien Vignol <sebastien@vignol.be>
2019-01-22 11:29:56 +0100

    '$' to 'jQuery' for compatibility

--- component.js
+++ component.js
@@ -1,12 +1,12 @@
 var Component = function(elem)
 {
     // DOM.
-    this.$elem  = $(elem);
+    this.$elem  = jQuery(elem);
     this.$child = this.$elem.find('.some-selector');
  
     // Events.
-    // Using $.proxy allows to access Component properties with 'this' in called fn.
-    this.$child.on('click', $.proxy(this.anyFunction, this));
+    // Using jQuery.proxy allows to access Component properties with 'this' in called fn.
+    this.$child.on('click', jQuery.proxy(this.anyFunction, this));
 };
 
 
@@ -17,12 +17,12 @@ Component.prototype.anyFunction = function(event)
 };
 
 
-$(document).ready(function()
+jQuery(document).ready(function()
 {
     // Creates one instance per component,
     // allows to have the same component multiple times on same page.
     // Preference: target a data attribute for distinction between style and functionality.
-    $('[data-component="mycomponent"]').each(function() {
+    jQuery('[data-component="mycomponent"]').each(function() {
         var comp = new Component(this);
     });
 });
